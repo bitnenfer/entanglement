b4e72df458fc9738fdf673284c01d6a05057b0d5
Felipe Alfonso <f.alfonso.espinosa@gmail.com>
2018-01-11 23:18:46 -0300

    camera update

--- entanglement/code/utils/camera.h
+++ entanglement/code/utils/camera.h
@@ -17,12 +17,12 @@ typedef struct camera camera_t;
 void camera_init();
 camera_t* camera_get();
 mat4_t camera_get_mvp(mat4_t* p_model);
-mat4_t camera_get_projection();
-mat4_t camera_get_view();
+mat4_t* camera_get_projection();
+mat4_t* camera_get_view();
 void camera_set_perspective(float32_t fov_y, float32_t aspect, float32_t n, float32_t f);
 void camera_set_ortho(float32_t left, float32_t right, float32_t bottom, float32_t top, float32_t n, float32_t f);
-void camera_look_at(vec3_t point);
-void camera_set_rotation(vec3_t rotation);
-void camera_set_position(vec3_t position);
+void camera_look_at(vec3_t* p_point);
+void camera_set_rotation(vec3_t* p_rotation);
+void camera_set_position(vec3_t* p_position);
 
 #endif // !_UTILS_CAMERA_H_
